0fc384402404b69cb3cb623885b0840747f63131
freyahu131 <freyahu131@gmail.com>
2018-03-19 18:49:29 +0800

    Fix gub:
    longitude and latitude are ok!
    
    Login UI revised

--- CarPool/Controller/ShareViewController/PassengerSetupViewController.swift
+++ CarPool/Controller/ShareViewController/PassengerSetupViewController.swift
@@ -18,12 +18,7 @@ class PassengerSetupViewController: UIViewController {
         guard DestinationPassenger.text != "" else{
             return
         }
-//        guard BoardingPoint.text != "" else{
-//            return
-//        }
-//        guard PeopleNumber.text != "" else{
-//            return
-//        }
+        
         let DesitinationAddress = DestinationPassenger.text
         let geoCoder = CLGeocoder()
         geoCoder.geocodeAddressString(DesitinationAddress!) { (placemarks, error) in
@@ -38,46 +33,41 @@ class PassengerSetupViewController: UIViewController {
             }
             // Use your location
             print(location)
-            let LocationString = String(describing: location)
-//            let String = "Location: < 37.78583400,-122.40641700> /- 5.00m (speed
-//            -1.00 mps / course -1.00) @ 5/8/17, 10:26:50 PM Pacific Daylight
-//            Time"
-            
-            let latLongString = LocationString.components(separatedBy: "< ")[1].components(separatedBy: ">")[0]
-            
-            let lat = latLongString.components(separatedBy: ",")[0]
-            let long = latLongString.components(separatedBy: ",")[1]
-            if let latitude =  Double(lat), let longitude = Double(long) {
-                let coordinate:CLLocation = CLLocation(latitude: latitude, longitude: longitude)
-                print(latitude ,longitude)
-                
-            }
-            print(5677)
-            let defaults = UserDefaults.standard
-            defaults.set(location, forKey: "location")
-            defaults.synchronize()
-            print(defaults.string(forKey: "location"))
+            print(location.coordinate.latitude)
+            print(location.coordinate.longitude)
+            //            print(5677)
+            //            let defaults = UserDefaults.standard
+            //            defaults.set(location, forKey: "location")
+            //            defaults.synchronize()
+            //            print(defaults.string(forKey: "location"))
             self.showAlert(message: "Yes! ")
             // Use your location
         }
         
-//        let savePassengerRecord = Trip(tripId: "", memberNo: 0,  destination: "", boarding: "", people: 0, onMap:0 , status:0 , date: "", boardingLat: 0.0, boardingLon: 0.0, destinationLat: 0.0, destinationLon: 0.0)
-//
-//        savePassengerRecord.destination = DestinationPassenger.text!
-//        savePassengerRecord.boarding = BoardingPoint.text!
-//        savePassengerRecord.people = Int(PeopleNumber.text!)!
-//
-//        Communicator.shared.modifyTrip(savePassengerRecord, mode: "C") { (error, result) in
-//            if let error = error {
-//                NSLog("doRegister fail: \(error)")
-//
-//                NSLog("乘客發起失敗")
-//                self.showAlert(message: "發起失敗")
-//            }
-//            // success
-//            NSLog("乘客發起成功")
-//            self.showAlert(message: "發起成功")
-//        }
+        //        guard BoardingPoint.text != "" else{
+        //            return
+        //        }
+        //        guard PeopleNumber.text != "" else{
+        //            return
+        //        }
+        
+        //        let savePassengerRecord = Trip(tripId: "", memberNo: 0,  destination: "", boarding: "", people: 0, onMap:0 , status:0 , date: "", boardingLat: 0.0, boardingLon: 0.0, destinationLat: 0.0, destinationLon: 0.0)
+        //
+        //        savePassengerRecord.destination = DestinationPassenger.text!
+        //        savePassengerRecord.boarding = BoardingPoint.text!
+        //        savePassengerRecord.people = Int(PeopleNumber.text!)!
+        //
+        //        Communicator.shared.modifyTrip(savePassengerRecord, mode: "C") { (error, result) in
+        //            if let error = error {
+        //                NSLog("doRegister fail: \(error)")
+        //
+        //                NSLog("乘客發起失敗")
+        //                self.showAlert(message: "發起失敗")
+        //            }
+        //            // success
+        //            NSLog("乘客發起成功")
+        //            self.showAlert(message: "發起成功")
+        //        }
         
     }
     
