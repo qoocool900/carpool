bdb3bf9da5907bf8e38cd01c89bcb23c2a0df810
freyahu131 <freyahu131@gmail.com>
2018-03-22 13:15:59 +0800

    DriverSetTableViewCell

--- CarPool/Controller/ShareViewController/DRecordingTableViewCell.swift
+++ CarPool/Controller/ShareViewController/DRecordingTableViewCell.swift
@@ -8,18 +8,41 @@
 import UIKit
 
 class DRecordingTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var driverBoarding: UILabel!
+    
+    @IBOutlet weak var driverDestination: UILabel!
+    
+    @IBOutlet weak var driverCapacity: UILabel!
+    
+    @IBOutlet weak var driverFee: UILabel!
+    
+    @IBOutlet weak var driverSetdate: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
     
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
+    var recordData: DriverSetRecord? {
+        didSet {
+            
+            
+            driverBoarding.text = recordData?.driverDeparture
+            driverDestination.text = recordData?.driverDestination
+            driverCapacity.text = String(describing: recordData?.driverSetPeople)
+            driverFee.text = String(describing:recordData?.driverSetFee)
+            driverSetdate.text = recordData?.driverSetDate
+            
+        }
+    }
+    
 }
